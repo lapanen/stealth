ec3f054a5aa24c0eb56b4d73fa5d16e2e489b448
Pedro Lapanen <lapanen@lapanen.nosuchtld>
2014-11-19 22:26:03 +0200

    Re-format.

--- stealth-maven/src/main/java/org/lapanen/stealth/maven/artifact/Dependency.java
+++ stealth-maven/src/main/java/org/lapanen/stealth/maven/artifact/Dependency.java
@@ -1,7 +1,7 @@
 package org.lapanen.stealth.maven.artifact;
 
 public interface Dependency extends java.io.Serializable {
-    java.lang.String DEFAULT_SCOPE = "compile";
+    String DEFAULT_SCOPE = "compile";
 
     Artifact getDependency();
 
