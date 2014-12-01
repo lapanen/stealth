64f9b0d40d8ab44d1968b3beae1ce17ab4fb59a0
Pedro Lapanen <lapanen@lapanen.nosuchtld>
2014-12-01 11:49:22 +0200

    Remove redundant method.

--- stealth-jar-signing-mongodb/src/main/java/org/lapanen/stealth/signing/repository/mongodb/MongoDbArtifactSigningRepository.java
+++ stealth-jar-signing-mongodb/src/main/java/org/lapanen/stealth/signing/repository/mongodb/MongoDbArtifactSigningRepository.java
@@ -6,8 +6,4 @@ import org.lapanen.stealth.signing.repository.ArtifactSigningRepository;
 import org.springframework.data.domain.Sort;
 
 public interface MongoDbArtifactSigningRepository extends ArtifactSigningRepository<MongoDbArtifactSigning> {
-
-    List<MongoDbArtifactSigning> findByGroupIdAndArtifactIdAndVersion(String groupId, String artifactId, String version);
-
-    List<MongoDbArtifactSigning> findByGroupIdAndArtifactIdAndVersion(String groupId, String artifactId, String version, Sort sort);
 }
